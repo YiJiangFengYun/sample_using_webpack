b159635cfded2dc8348b7387661923628d49eab5
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-12-03 18:25:37 +0800

    Add first content. 1. Add npm project package. 2. Add content of bunding html, js and css.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // instead of style-loader
+                    'css-loader'
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    mode: 'development',
+}
